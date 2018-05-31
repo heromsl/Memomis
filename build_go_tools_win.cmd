c3fa623acf6edd8d9572026be414a7eb5af43825
heromsl <heromsl@gmail.com>
2018-05-31 18:41:33 +0800

    Create build_go_tools_win.cmd

---
+++ build_go_tools_win.cmd
@@ -0,0 +1,9 @@
+go get -u -v github.com/derekparker/delve/cmd/dlv
+go get -u -v github.com/uudashr/gopkgs/cmd/gopkgs
+go get -u -v golang.org/x/lint/golint
+go get -u -v golang.org/x/tools/cmd/guru
+go get -u -v github.com/newhook/go-symbols
+go get -u -v github.com/nsf/gocode
+go get -u -v github.com/rogpeppe/godef
+go get -u -v golang.org/x/tools/cmd/gorename
+go get -u -v github.com/cweill/gotests/...
