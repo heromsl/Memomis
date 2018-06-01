d0f4095570028a21c9ee5789e31cdd11cb427121
heromsl <heromsl@gmail.com>
2018-06-01 11:51:40 +0800

    Update build_go_tools_win.cmd

--- build_go_tools_win.cmd
+++ build_go_tools_win.cmd
@@ -1,3 +1,6 @@
+@echo off
+rem set http_proxy=http://127.0.0.1:1080
+rem set https_proxy=http://127.0.0.1:1080
 go get -u -v github.com/derekparker/delve/cmd/dlv
 go get -u -v github.com/uudashr/gopkgs/cmd/gopkgs
 go get -u -v golang.org/x/lint/golint
@@ -7,3 +10,4 @@ go get -u -v github.com/nsf/gocode
 go get -u -v github.com/rogpeppe/godef
 go get -u -v golang.org/x/tools/cmd/gorename
 go get -u -v github.com/cweill/gotests/...
+go get -u -v github.com/ramya-rao-a/go-outline
